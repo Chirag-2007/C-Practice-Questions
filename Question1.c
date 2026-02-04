9ca45edb05156bf94d91398b5e1872e7cb60c22e
C-N-2007 <nagpalchirag757@gmail.com>
2026-02-04 19:36:16 +0530

    Question1

---
+++ Question1.c
@@ -0,0 +1,12 @@
+// Take two numbers as input and print their sum.
+
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d",&x);
+    int y;
+    scanf("%d",&y);
+    int total = x + y;
+    printf("%d",total);
+    return 0;
+}
