d75946c990537bb6b31f55ed41a7fea82ed46b41
C-N-2007 <nagpalchirag757@gmail.com>
2026-03-19 20:03:45 +0530

    Question46

---
+++ Question46.c
@@ -0,0 +1,19 @@
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int x = 1;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d ",x);
+            x++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
