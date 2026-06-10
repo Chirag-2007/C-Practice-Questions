7eb8b8ff64aefe3ce42fec6a0d0ebe12d4e9b8fd
C-N-2007 <nagpalchirag757@gmail.com>
2026-06-10 21:33:27 +0530

    Question147

---
+++ Question147.c
@@ -0,0 +1,19 @@
+// Q7.  Add two numbers
+// Problem statement
+// Given two numbers, add them and print their sum
+// Input
+// Input contains two numbers between 1 and 100, separated by a space
+// Output
+// Print the sum of the 2 numbers
+// Sample Input
+// 1 2
+// Sample Output
+// 3
+
+#include<stdio.h>
+int main(){
+    int n, m;
+    scanf("%d %d",&n,&m);
+    printf("%d",n + m);
+    return 0;
+}
