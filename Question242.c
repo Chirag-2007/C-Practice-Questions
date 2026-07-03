0faa2d533ed315fc3571ded6052ccec538bbd8ab
C-N-2007 <nagpalchirag757@gmail.com>
2026-07-03 05:33:49 +0530

    Question242

---
+++ Question242.c
@@ -0,0 +1,26 @@
+// Problem Statement 56
+// Aman wants to print enum values
+// Write a program that:
+// ● Defines an enum with 3 values
+// ● Prints all values
+// Sample Test Case 1
+// Input:
+// (no input)
+// Output:
+// 0 1 2
+// Sample Test Case 2
+// Input:
+// (no input)
+// Output:
+// 0 1 2
+// Hidden Test Cases
+// Test Case | Input | Output
+
+#include<stdio.h>
+
+enum day {Mon,Tue,Wed};
+
+int main(){
+    printf("%d %d %d",Mon,Tue,Wed);
+    return 0;
+}
