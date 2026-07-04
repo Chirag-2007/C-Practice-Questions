5480c2204d4216603c0615c14813117909c7d76c
C-N-2007 <nagpalchirag757@gmail.com>
2026-07-04 16:15:19 +0530

    Question247

---
+++ Question247.c
@@ -0,0 +1,23 @@
+// Problem Statement 61
+// Aman wants to understand auto storage class
+// Write a program that:
+// ● Uses auto variable
+// ● Prints value
+// Sample Test Case 1
+// Input:
+// 5
+// Output:
+// 5
+// Sample Test Case 2
+// Input:
+// 10
+// Output:
+// 10
+
+#include<stdio.h>
+int main(){
+    auto int x;
+    scanf("%d",&x);
+    printf("%d",x);
+    return 0;
+}
